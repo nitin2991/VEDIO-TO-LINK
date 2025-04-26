32fde53932f9efd5865d36f0c7b48500f44d3c88
nitin2991 <nitin.kamboj510@gmail.com>
2025-04-26 23:41:16 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -40,7 +40,7 @@ async def batch(client: Client, message: Message):
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 𝚂𝚑𝚊𝚛𝚎 𝚄𝚁𝙻", url=f'https://telegram.me/share/url?url={link}')]])
-    await second_message.reply_text(f"<b><blockquote><bold>🧑‍💻 𝙷𝚎𝚛𝚎 𝙸𝚜 𝚈𝚘𝚞𝚛 𝙲𝚘𝚍𝚎.<blockquote><bold>\n\n<blockquote><bold>𝚄𝚙𝚕𝚘𝚊𝚍𝚎𝚍 𝙱𝚢 @DD_FREE_DISHH\n\n<code><blockquote><bold>{base64_string}</blockquote></bold></code></blockquote></bold></b>\n\n<b><blockquote><bold>📤𝙷𝚎𝚛𝚎 𝙸𝚜 𝚈𝚘𝚞𝚛 𝙳𝚘𝚠𝚗𝚕𝚘𝚊𝚍 𝙻𝚒𝚗𝚔📥</blockquote></bold>\n\n<blockquote><bold>𝚄𝚙𝚕𝚘𝚊𝚍𝚎𝚍 𝙱𝚢 @dd_free_dishh</blockquote></bold></b>\n\n</blockquote><bold>{link}</blockquote></bold>", quote=True, reply_markup=reply_markup)
+    await second_message.reply_text(f"<b><blockquote><bold>🧑‍💻 𝙷𝚎𝚛𝚎 𝙸𝚜 𝚈𝚘𝚞𝚛 𝙲𝚘𝚍𝚎.<blockquote><bold>\n\n<blockquote><bold>𝚄𝚙𝚕𝚘𝚊𝚍𝚎𝚍 𝙱𝚢 @Nitin_510\n\n<code><blockquote><bold>{base64_string}</blockquote></bold></code></blockquote></bold></b>\n\n<b><blockquote><bold>📤𝙷𝚎𝚛𝚎 𝙸𝚜 𝚈𝚘𝚞𝚛 𝙳𝚘𝚠𝚗𝚕𝚘𝚊𝚍 𝙻𝚒𝚗𝚔📥</blockquote></bold>\n\n<blockquote><bold>𝚄𝚙𝚕𝚘𝚊𝚍𝚎𝚍 𝙱𝚢 @dd_free_dishh</blockquote></bold></b>\n\n</blockquote><bold>{link}</blockquote></bold>", quote=True, reply_markup=reply_markup)
 
 
 
