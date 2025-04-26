af36fc23f336346cdfa84216d1e5e97df439fa82
nitin2991 <nitin.kamboj510@gmail.com>
2025-04-26 18:42:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ PORT = os.environ.get("PORT", "6245")
 #your database url mongodb only You can use mongo atlas free cloud database
 DB_URL = os.environ.get("DB_URL", "mongodb+srv://nitin:nitin000001@sharebot.tcyjilw.mongodb.net/?retryWrites=true&w=majority&appName=sharebot")
 #your database name
-DB_NAME = os.environ.get("DB_NAME", nitin")
+DB_NAME = os.environ.get("DB_NAME", "nitin")
 
 #for creating telegram thread for bot to improve performance of the bot
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "100"))
